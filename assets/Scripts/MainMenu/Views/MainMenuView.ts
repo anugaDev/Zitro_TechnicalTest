4d7a3a9e1adcc8501c24e4e1808b70568897cd66
Adrián Núñez Garrido <adrinuga@gmail.com>
2026-04-29 16:06:18 +0200

    fixed scene transition

--- assets/Scripts/MainMenu/Views/MainMenuView.ts
+++ assets/Scripts/MainMenu/Views/MainMenuView.ts
@@ -4,8 +4,7 @@ import { IMainMenuView } from './IMainMenuView';
 const { ccclass, property } = _decorator;
 
 @ccclass('MenuView')
-export class MainMenuView extends Component implements IMainMenuView
-{
+export class MainMenuView extends Component implements IMainMenuView {
     @property(RichText)
     public ClockText: RichText = null!
 
@@ -15,30 +14,31 @@ export class MainMenuView extends Component implements IMainMenuView
     @property(Button)
     public SlotButton: Button = null!;
 
-    public updateClock(time: string): void
-    {
+    public updateClock(time: string): void {
         this.ClockText.string = time;
     }
 
-    public setButtonsInteractable(value: boolean): void
-    {
+    public setButtonsInteractable(value: boolean): void {
         this.QuizButton.interactable = value;
         this.SlotButton.interactable = value;
     }
 
-    public bindQuizButton(handler: () => void): void
-    {
+    public bindQuizButton(handler: () => void): void {
         this.QuizButton.node.on(Button.EventType.CLICK, handler, this);
     }
 
-    public bindSlotButton(handler: () => void): void
-    {
+    public bindSlotButton(handler: () => void): void {
         this.SlotButton.node.on(Button.EventType.CLICK, handler, this);
     }
 
-    public unbindAll(): void
-    {
-        this.QuizButton.node.off(Button.EventType.CLICK);
-        this.SlotButton.node.off(Button.EventType.CLICK);
+    public unbindAll(): void {
+        if (this.QuizButton && this.QuizButton.node)
+        {
+            this.QuizButton.node.off(Button.EventType.CLICK);
+        }
+        if (this.SlotButton && this.SlotButton.node)
+        {
+            this.SlotButton.node.off(Button.EventType.CLICK);
+        }
     }
 }
